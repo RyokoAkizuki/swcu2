e700fb59c2b5d525bfc6e681565e6ea9f3e1af2b
Yukino Hayakawa <tennencoll@gmail.com>
2014-12-20 15:28:01 +0800

    ensure that the player is logged in

--- SAMP/Server.cpp
+++ SAMP/Server.cpp
@@ -168,6 +168,20 @@ PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerUpdate(int playerid)
     {
         return false;
     }
+    if(!p->isLoggedIn())
+    {
+        if(p->isRegistered())
+        {
+            swcu::DialogManager::get().push
+                <swcu::PlayerLoginDialog>(playerid);
+        }
+        else
+        {
+            swcu::DialogManager::get().push
+                <swcu::PlayerRegisterDialog>(playerid);
+        }
+        return false;
+    }
     if(p->hasFlags(swcu::STATUS_JAILED))
     {
         if(p->isPrisonTermExceeded())
